01aceaba628320f93d50b51c4707ec561e215d1a
John Ndungu <ndungulink@gmail.com>
2025-04-20 08:47:58 +0300

    Fetching products from the DummyJSON API

--- main.js
+++ main.js
@@ -147,3 +147,29 @@ let getIndexKenya = personArray.findIndex((singlePerson) => {
 });
 
 console.log(getIndexKenya);
+
+let getListOfproductsElement = document.querySelector(".list-of-products");
+
+function renderProducts(getProducts) {
+  getListOfproductsElement.innerHTML = getProducts
+    .map((singleProductItem) => `<p>${singleProductItem.title}</p>`)
+    .join(" ");
+}
+
+async function fetchListOfProducts() {
+  try {
+    const apiResponse = await fetch("https://dummyjson.com/products", {
+      method: "GET",
+    });
+
+    const result = await apiResponse.json();
+
+    console.log(result);
+
+    if (result?.products?.length > 0) renderProducts(result?.products);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+fetchListOfProducts();
